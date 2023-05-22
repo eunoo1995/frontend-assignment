e6ecce68a1b088ef91f44655c0bf14dd3325c5eb
eunoo1995 <eunoo1995@gmail.com>
2023-05-22 16:09:51 +0900

    Feat: add products, products/:id page heading

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,3 +1,14 @@
+import { ProductCard } from "../components/product/ProductCard";
+import Box from "@mui/material/Box";
+import { visuallyHidden } from "@mui/utils";
+
 export const ProductPage = () => {
-  return <div>Product</div>;
+  return (
+    <>
+      <Box sx={visuallyHidden}>
+        <h2>Product Detail</h2>
+      </Box>
+      <ProductCard />
+    </>
+  );
 };
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,5 +1,14 @@
 import { ProductList } from "../components/products/ProductList";
+import Box from "@mui/material/Box";
+import { visuallyHidden } from "@mui/utils";
 
 export const ProductsPage = () => {
-  return <ProductList />;
+  return (
+    <>
+      <Box sx={visuallyHidden}>
+        <h2>Product List</h2>
+      </Box>
+      <ProductList />
+    </>
+  );
 };
