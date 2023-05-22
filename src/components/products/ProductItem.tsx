65563a65cdc2d1e23135e1bd7e47c9a43b51d06b
eunoo1995 <eunoo1995@gmail.com>
2023-05-22 16:20:56 +0900

    Refactor: Product price notation change

--- src/components/products/ProductItem.tsx
+++ src/components/products/ProductItem.tsx
@@ -2,19 +2,29 @@ import styled from "@emotion/styled";
 import { Product } from "../../@types/interface";
 import { Link } from "react-router-dom";
 
+const Li = styled.li`
+  margin-bottom: 8px;
+`;
+
 const StyledImg = styled.img`
   width: 250px;
   height: 250px;
 `;
 
+const Text = styled.p`
+  margin: 0;
+  font-weight: bold;
+  padding-top: 4px;
+`;
+
 export const ProductItem = (product: Product) => {
   return (
-    <li>
+    <Li>
       <Link to={`${product.id}`}>
         <StyledImg src={product.imageURL} alt="" />
-        <h3>{product.title}</h3>
-        <span>{product.price}</span>
+        <Text>{product.title}</Text>
+        <Text>{`₩ ${Number(product.price).toLocaleString()}`}</Text>
       </Link>
-    </li>
+    </Li>
   );
 };
