4ec888a760e7eeefe60fcab7f4a08b89d62528e4
eunoo1995 <eunoo1995@gmail.com>
2023-05-22 14:36:21 +0900

    Feat: react-query setup

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,25 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { CssBaseline, GlobalStyles } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <BrowserRouter>
     <CssBaseline />
-    <GlobalStyles styles={{ body: { backgroundColor: "#fefefe" } }} />
-    <App />
+    <GlobalStyles
+      styles={{
+        body: { backgroundColor: "#fefefe", color: "#222" },
+        a: { textDecoration: "none", color: "inherit" },
+      }}
+    />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </BrowserRouter>
 );
