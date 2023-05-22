d9447c65d898f35a7cbdd730decae65d615dc0b9
eunoo1995 <eunoo1995@gmail.com>
2023-05-22 13:21:05 +0900

    Feat: add css normalize and globalStyles

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { CssBaseline, GlobalStyles } from "@mui/material";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <BrowserRouter>
+    <CssBaseline />
+    <GlobalStyles styles={{ body: { backgroundColor: "#fefefe" } }} />
     <App />
   </BrowserRouter>
 );
